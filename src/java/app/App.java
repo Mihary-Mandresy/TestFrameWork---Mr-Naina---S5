7282cae98c297632fb7d8514bb3a25cada019248
Mihary <miharymandresy12@gmail.com>
2025-10-24 16:57:24 +0300

    feat: sprint 2

--- src/java/app/App.java
+++ src/java/app/App.java
@@ -1,8 +1,30 @@
 package app;
 
+import java.lang.reflect.Method;
+
+import com.mhframework.annotation.UrlMapping;
+
+import controller.TestController;
+
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Word !");
+        String path = "/bye";
+        try {
+            TestController testController = new TestController();
+            Class<?> cls = testController.getClass();
+
+            for (Method method : cls.getDeclaredMethods()) {
+                UrlMapping urlMapping = method.getAnnotation(UrlMapping.class);
+                if (urlMapping != null) {
+                    String url = urlMapping.value();
+                    if (url.equals(path)) {
+                        method.invoke(testController);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
